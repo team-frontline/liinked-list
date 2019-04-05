03e70f8a9538161c8751a26a839a102ee7f349d0
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-04-05 22:03:58 +0530

    main method with thread creation and other initial parts

--- linked_list_impl.c
+++ linked_list_impl.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <pthread.h>
 
 #define MAX_THREADS 1024
@@ -13,11 +14,15 @@ float m_insert_frac = 0.0, m_delete_frac = 0.0, m_member_frac = 0.0;
 // Total number of each operation
 float m_insert = 0.0, m_delete = 0.0, m_member = 0.0;
 
+struct node *first_node;
+
 struct node
 {
    int val;
    struct node *next_node;
 };
+
+void gen_linked_list();
 
 int Insert(int value, struct node **head_pp);
 
@@ -37,6 +42,11 @@ int main(int argc, char *argv[])
    m_insert_frac = 0.4;
    m_delete_frac = 0.4;
 
+   // Calculating the total number od each operation
+   m_insert = m_insert_frac * m;
+   m_delete = m_delete_frac * m;
+   m_member = m_member_frac * m;
+
    //TODO: Validate Arguments
 
    //validate args
@@ -49,9 +59,28 @@ int main(int argc, char *argv[])
    //Test
    printf("%d %d %d\n", n, m, thread_count);
 
+   pthread_mutex_t mutex;
+
    //Thread handlers initiating
    pthread_t *thread_handlers;
    thread_handlers = malloc(sizeof(pthread_t) * thread_count);
+
+   // time variables
+   struct timeval time_begin, time_end;
+
+   // Initializing the mutex
+   pthread_mutex_init(&mutex, NULL);
+
+   // Getting the begin time stamp
+   gettimeofday(&time_begin, NULL);
+
+   // Getting the end time stamp
+   gettimeofday(&time_end, NULL);
+
+   // Destroying the mutex
+   pthread_mutex_destroy(&mutex);
+
+   printf("Linked List with a single mutex Time Spent : %.6f secs\n", CalcTime(time_begin, time_end));
    return 0;
 }
 
@@ -70,4 +99,15 @@ int Member(int value, struct node *first_node)
    }
 
    return 0;
+}
+
+void gen_linked_list()
+{
+   // Linked List Generation with Random values
+   int i = 0;
+   while (i < n)
+   {
+      if (Insert(rand() % 65535, &first_node) == 1)
+         i++;
+   }
 }
