f4fae5270b440227403ddb62fb9902658a063063
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-04-05 15:28:25 +0530

    linked list insert method implemented

--- serial_linked_list.c
+++ serial_linked_list.c
@@ -22,5 +22,32 @@ int Member(int value, struct linked_list_node **list_head);
 
 int Insert(int value, struct linked_list_node **list_head)
 {
-    
+    struct linked_list_node *current_pointer = *list_head;
+    struct linked_list_node *predecer_pointer = NULL;
+    struct linked_list_node *temp_pointer = NULL;
+
+    while (current_pointer != NULL && current_pointer->value < value)
+    {
+        predecer_pointer = current_pointer;
+        current_pointer = current_pointer->next_node;
+    }
+
+    if (current_pointer == NULL || current_pointer->value > value)
+    {
+        temp_pointer = malloc(sizeof(struct linked_list_node));
+        temp_pointer->value = value;
+        temp_pointer->next_node = current_pointer;
+
+        if (predecer_pointer == NULL)
+        {
+            *list_head = temp_pointer;
+        }
+        else
+            predecer_pointer->next_node = temp_pointer;
+    }
+    else
+    {
+        printf("Can not insert the same value, " + value);
+    }
+    return 0;
 }
